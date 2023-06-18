fc83146dc2fe106706bd80dc95ee10fab0c187f9
volunteerchallenge <136908427+volunteerchallenge@users.noreply.github.com>
2023-06-18 21:35:30 +0800

    upload js

---
+++ js/setting.js
@@ -0,0 +1,11 @@
+<!-- 这是网站所需的的链接和设置 -->
+document.writeln("    <meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'>");
+<!-- 适应移动端bootstrap中的grid -->
+document.writeln("        <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'>");
+<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+document.writeln("        <link rel=\'stylesheet\' href=\'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css\' integrity=\'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\' crossorigin=\'anonymous\'>");
+<!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
+document.writeln("		<link rel=\'stylesheet\' href=\'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css\' integrity=\'sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\' crossorigin=\'anonymous\'>");
+document.writeln("		<script src=\'https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js\'></script>");
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+document.writeln("        <script src=\'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js\' integrity=\'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\' crossorigin=\'anonymous\'></script>");
